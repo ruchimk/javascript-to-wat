ecb538f54929a7bc795496a937247dc5adef4073
Ruchi Malik <rmalik2788@gmail.com>
2014-11-23 20:48:42 -0500

    first chrome extension

---
+++ README.md
@@ -0,0 +1,15 @@
+##Javascript to Wat
+=============
+
+Chrome extension that replaces occurrences of 'javascript' with 'wat'
+
+Note that there are forks of this extension that simply replace 'cloud' with 'butt'.
+In my personal opinion, that approach is too broad and it's less funny as a result, but it is clearly a very
+polarizing issue in the cloud-to-butt user community.  Forks are free to do whatever they like.  But officially, _this_
+extension replaces only the phrase described above. Thank you for your concern.
+
+
+##Credits
+
+Copied from Steven Frank's [Cloud-To-Butt](https://github.com/panicsteve/cloud-to-butt) extension.
+Inspired from Gary Bernhardt's video [Wat](https://www.youtube.com/watch?v=Othc45WPBhA).
---
+++ source/content_script.js
@@ -0,0 +1,39 @@
+walk(document.body);
+
+function walk(node)
+{
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
+
+  var child, next;
+
+  switch ( node.nodeType )
+  {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while ( child )
+      {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
+}
+
+function handleText(textNode){
+  var v = textNode.nodeValue;
+
+  v = v.replace(/\bJavascript\b/g, "Wat");
+  v = v.replace(/\bjavascript\b/g, "wat");
+  v = v.replace(/\bJAVASCRIPT\b/g, "WAT");
+
+  textNode.nodeValue = v;
+}
+
